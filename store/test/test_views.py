fb65f1bc4324bb37640d83a848031f40657cc511
eselikburak <eselik.burak@gmail.com>
2024-10-22 14:23:06 +0300

    first lesson ends - initial version

---
+++ store/test/test_views.py
@@ -0,0 +1,63 @@
+from unittest import skip
+
+from django.contrib.auth.models import User
+from django.http import HttpRequest
+from django.test import Client, RequestFactory, TestCase
+from django.urls import reverse
+
+from store.models import Category, Product
+from store.views import all_products
+
+
+class TestViewResponses(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.factory = RequestFactory()
+        User.objects.create(username='admin')
+        Category.objects.create(name='django', slug='django')
+        Product.objects.create(category_id=1, title='django begginers', created_by_id=1, slug='django-begginers', 
+                               price='20.00', image='django')
+
+    def test_url_allowed_hosts(self):
+        """
+        Test Allowed Host
+        """
+        response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_url(self):
+        """
+        Test Product response status
+        """
+        response = self.c.get(reverse('store:product_detail', args=['django-begginers']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_detail_url(self):
+        """
+        Test Category response status
+        """
+        response = self.c.get(reverse('store:category_list', args=['django']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_html(self):
+        """
+        Example: code validation, search HTML for text
+        """
+        request = HttpRequest()
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_function(self):
+        """
+        Example: Using request factory
+        """
+        request = self.factory.get('/item/django-beginners')
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
